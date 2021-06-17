9321bc69ab50c0fbeea8872d21c6c91da48f08c2
hospice-g <76434345+hospice-g@users.noreply.github.com>
2021-06-17 15:05:30 +0100

    Add files via upload

---
+++ DicoNote_Hospice.py
@@ -0,0 +1,8 @@
+#Dictionnaire
+hospice ={'Matières':['Java','Analyse', 'Se', 'Anglais', 'Langage C', 'reseaux','UML'],'Note':[18,20,12,14,13,15,14]}
+
+print(hospice)
+
+moy = (hospice['Note'][0]+hospice['Note'][1]+hospice['Note'][2]+hospice['Note'][3]+hospice['Note'][4]+hospice['Note'][5]+hospice['Note'][6])/7
+
+print(moy)
